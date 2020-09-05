8b0bbd2345758798617c2315a4b2fa8556133911
lmitiche <61794558+lmitiche@users.noreply.github.com>
2020-09-04 22:16:09 -0400

    update

--- labs/labs1/app.js
+++
@@ -1,6 +0,0 @@
-let myName = "A. Name";
-let age = 42;
-console.log(myName);
-
-myName = "Same variable, different name.";
-console.log(myName);
---
+++ labs/labs1/js/app.js
@@ -0,0 +1,31 @@
+function setup() {
+
+    var startX = 235;
+    var white = "255, 255, 255"
+
+    createCanvas(800,600);
+    background(175, 199, 195);
+
+    
+	fill(white);
+	ellipse(250, 500, 200, 200);
+	ellipse(250, 375, 150, 150);
+    ellipse(250, 275, 100, 100);
+    
+    
+    fill(17, 18, 18)
+	ellipse(startX, 265, 5, 5);
+    ellipse(265, 265, 5, 5);
+    
+    line(180, 375, 100, 350);
+    line(320, 375, 400, 350);
+    
+    
+	
+    
+
+    
+
+   
+    
+}
