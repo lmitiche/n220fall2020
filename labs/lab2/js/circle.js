bf69923f8da3e14f060b299f3144fcf5567f5b06
lmitiche <61794558+lmitiche@users.noreply.github.com>
2020-09-14 13:22:57 -0400

    update

---
+++ labs/lab2/js/circle.js
@@ -0,0 +1,14 @@
+let d = 1
+
+function setup(){
+    createCanvas(500,500);
+}
+function draw(){
+    background(0,100,100);
+        circle (250,250,d);
+        d = d + 1;
+    if (d == 200)[
+        d = 1
+    ]
+
+}
