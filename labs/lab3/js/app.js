2897efc1f3f3b7bef6f41728d6871f88cc473e58
lmitiche <61794558+lmitiche@users.noreply.github.com>
2020-09-20 03:27:02 -0400

    update

--- labs/lab3/js/app.js
+++ labs/lab3/js/app.js
@@ -1,13 +1,16 @@
 function setup() {
     createCanvas(400,300);
+
 }
+
 
 function draw() {
-    background(000);
-}
+    background(100);
+
 
     //For loop syntax
-    for( var i = 0; i  < 3; i++){
+    for( var i =0; i < 3; i++) {
         circle(100, 100, i * 30);
     }
     console.log("Afterwards: " + i);
+}
