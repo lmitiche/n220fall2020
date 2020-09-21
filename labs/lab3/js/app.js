e3ccb3993fa694834fa81ff2bc5f6912e06f2afa
lmitiche <61794558+lmitiche@users.noreply.github.com>
2020-09-20 23:27:36 -0400

    update

--- labs/lab3/js/app.js
+++ labs/lab3/js/app.js
@@ -8,9 +8,13 @@ function draw() {
     background(100);
 
 
+    noFill();
+
     //For loop syntax
-    for( var i =0; i < 3; i++) {
-        circle(100, 100, i * 30);
+    for( var i =0; i < 30; i++) {
+        stroke(255 - (i * 15));
+
+        circle(200,150, i * 5);
     }
     console.log("Afterwards: " + i);
 }
