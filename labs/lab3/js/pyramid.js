f7d6121082b9e03f48ab3f50b5073eaf594aa577
lmitiche <61794558+lmitiche@users.noreply.github.com>
2020-09-21 01:52:06 -0400

    update

--- labs/lab3/js/pyramid.js
+++ labs/lab3/js/pyramid.js
@@ -1,3 +1,22 @@
+let ypos = 140;
+let ystep = 30;
+let xpos = 80;
+let xstep = 30;
 function setup() {
-    createCanvas(400,300);
+  createCanvas(400, 300);
+  noLoop();
 }
+
+function draw() {
+  background(0,100,100);
+  fill(50);
+  noStroke();
+
+  for (let j = 0; j < 5; j++) {
+    for (let i = 0; i < 5; i++) {
+      ellipse(xpos+(xstep*i), ypos+(ystep*j), 20, 20);
+    }
+  }
+}
+
+
