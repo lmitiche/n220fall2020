47fcb7c3aeb0500bd161565a7cdc265896aa7dc8
lmitiche <61794558+lmitiche@users.noreply.github.com>
2020-09-25 19:45:15 -0400

    update

--- labs/lab4/js/water.js
+++ labs/lab4/js/water.js
@@ -7,34 +7,44 @@
 //     }
 // }
 
-// function setup(){
-// createCanvas(400,300);
 
-// }
+ //function setup(){
+      //createCanvas(400,300);
 
-// function draw(){
-//     background(0);
-//     let droplets = Drops;
-//     droplets.createCircle();
-//     droplets.y += droplets.velocityY;
+//}
+
+ //function draw(){
+     //background(0);
+     //let droplets = Drops;
+     //droplets.createCircle();
+     //droplets.y += droplets.velocityY;
    
-// }
+ //}
 
-let y = [0,0,0,0,0,0,0,0,0,0];
-let x = 200;
-let velY = 5;
+
+ let y = [0,0,0,0,0,0,0,0,0,0];
+ let x = 200;
+  let velY = 5;
+  
+
 function setup(){
-    createCanvas(400, 300)
+   createCanvas(400, 300)
 }
 
 function draw(){
     background(0)
-    for(let i = 0; i < y.length; i++){
+    
+
+    for(let i = 0; i < 10 ; i++){
         if(i % 10 == 0){
           y[i] += 5;  
+
         }
         
-        circle(x, y[i], 20)
+        circle(x, y[i], 20);
+        
     }
 
-}
+} 
+
+
