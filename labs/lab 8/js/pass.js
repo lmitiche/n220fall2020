ae2a3d87feccef757a79b1f4bdefe3a91a754d10
lmitiche <61794558+lmitiche@users.noreply.github.com>
2020-10-30 22:56:48 -0400

    update

--- labs/lab 8/js/pass.js
+++ labs/lab 8/js/pass.js
@@ -1,12 +1,12 @@
 let txtUsername = document.getElementById("txtUsername");
 let txtPassword = document.getElementById("txtPassword");
 let dvResult = document.getElementById("dvResults");
-dvResult.innerHTML = "Insert Password"
+dvResult.innerHTML = "Insert Password";
 
 function checkInput(){
     if( txtUsername.value == "Username" && txtPassword.value == "Password"){
-       dvResult.innerHTML = "Success"
+        dvResult.innerHTML = "Success";
     } else{
-        dvResult.innerHTML = "Wrong Information"
+        dvResult.innerHTML = "Wrong Information";
     }
 }
