f802da8555ad73235be55c71b54397e727e11c0e
lmitiche <61794558+lmitiche@users.noreply.github.com>
2020-11-06 23:13:47 -0500

    update

--- labs/lab9/js/app.js
+++ labs/lab9/js/app.js
@@ -3,6 +3,10 @@ let dvSquare = document.getElementById("square");
 dvSquare.style.backgroundColor = "#16de4b"
 dvSquare.style.height = "100px";
 dvSquare.style.width = "100px"; 
+dvSquare.style.marginTop = "50px";
+dvSquare.style.marginLeft = "50px";
+dvSquare.style.marginRight = "50px";
+dvSquare.style.marginBottom = "50px";
 
 
 
